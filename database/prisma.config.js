5e323c82dd97a2af9ce5ce8fee4c30cc52bde9f4
Ahmad NuR <ahmadnursahid12345@gmail.com>
2026-02-24 21:38:33 +0700

    feet: Setup database
    - Upgrade to Prisma 7
    - Delete the current schema
    - Add a new db:generate script to the backend

---
+++ database/prisma.config.js
@@ -0,0 +1,16 @@
+require("dotenv/config");
+const { defineConfig, env } = require("prisma/config");
+
+module.exports = defineConfig({
+  schema: "schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+  migrations: {
+    path: "/migrations",
+    seed: '/seeds'
+  },
+  views: {
+    path: '/views'
+  }
+});
